7010f45c72d39379bd9fd2c76a9669abdaf7c848
Peter Dotchev <dotchev@gmail.com>
2019-09-13 07:21:00 +0300

    [wip] Initial producer

--- rabbitmq/producer/src/main/java/Send.java
+++ rabbitmq/producer/src/main/java/Send.java
@@ -4,18 +4,17 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
 
-    private final static String QUEUE_NAME = "hello";
+  private final static String QUEUE_NAME = "hello";
 
-    public static void main(String[] argv) throws Exception {
-      System.out.println("Starting...");
-      ConnectionFactory factory = new ConnectionFactory();
-      factory.setHost("localhost");
-      try (Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel()) {
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello World!";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-        System.out.println(" [x] Sent '" + message + "'");
-      }
+  public static void main(String[] argv) throws Exception {
+    System.out.println("Starting...");
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost(System.getenv("RABBITMQ_HOST"));
+    try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      String message = "Hello World!";
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+      System.out.println(" [x] Sent '" + message + "'");
     }
+  }
 }
