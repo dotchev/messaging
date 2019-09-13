75fd0448f4bf965b779630b33077001f628c2263
Peter Dotchev <dotchev@gmail.com>
2019-09-13 16:32:13 +0300

    Producer and Consumer

---
+++ rabbitmq/consumer/src/main/java/Consumer.java
@@ -0,0 +1,29 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Consumer {
+
+  private final static String RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
+  private final static String QUEUE_NAME = System.getenv("QUEUE_NAME");
+
+  public static void main(String[] argv) throws Exception {
+    System.out.println("Connecting to RabbitMQ at host " + RABBITMQ_HOST);
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost(RABBITMQ_HOST);
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    System.out.println("Declaring queue " + QUEUE_NAME);
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println("Waiting for messages...");
+
+    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+      String message = new String(delivery.getBody(), "UTF-8");
+      System.out.println("Received message: " + message);
+    };
+    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+    });
+  }
+}
---
+++ rabbitmq/producer/src/main/java/Producer.java
@@ -0,0 +1,60 @@
+import java.rmi.ConnectIOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+
+  private final static String RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
+  private final static String QUEUE_NAME = System.getenv("QUEUE_NAME");
+  private final static String MESSAGE_PERIOD = System.getenv("MESSAGE_PERIOD");
+
+  private final static class Task extends TimerTask {
+    private Connection connection;
+    private Channel channel;
+    private long counter = 0;
+
+    private Channel connect() throws Exception {
+      if (channel != null && channel.isOpen()) {
+        return channel;
+      }
+
+      System.out.println("Connecting to RabbitMQ at host " + RABBITMQ_HOST);
+      ConnectionFactory factory = new ConnectionFactory();
+      factory.setHost(RABBITMQ_HOST);
+      connection = factory.newConnection();
+      channel = connection.createChannel();
+      System.out.println("Declaring queue " + QUEUE_NAME);
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      return channel;
+    }
+
+    @Override
+    public void run() {
+      try {
+        connect();
+        String message = "M" + counter++;
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println("Sent message: " + message);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] argv) throws Exception {
+    long period;
+    try {
+      period = Long.parseLong(MESSAGE_PERIOD);
+    } catch (Exception e) {
+      period = 5000;
+    }
+
+    Task task = new Task();
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(task, period, period);
+  }
+}
