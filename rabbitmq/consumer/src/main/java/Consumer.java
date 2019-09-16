0d6099335ab30d764e9bf7dea2c1a0a0287c9747
Peter Dotchev <dotchev@gmail.com>
2019-09-16 14:21:21 +0300

    Make consumer resilient to rabbitmq presence

--- rabbitmq/consumer/src/main/java/Consumer.java
+++ rabbitmq/consumer/src/main/java/Consumer.java
@@ -12,16 +12,11 @@ public class Consumer {
   private final static String QUEUE_NAME = System.getenv("QUEUE_NAME");
 
   public static void main(String[] argv) throws Exception {
-    System.out.println("Connecting to RabbitMQ at host " + RABBITMQ_HOST);
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost(RABBITMQ_HOST);
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
-
+    Channel channel = connect();
     System.out.println("Declaring queue " + QUEUE_NAME);
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    System.out.println("Waiting for messages...");
 
+    System.out.println("Waiting for messages...");
     DeliverCallback deliverCallback = (consumerTag, delivery) -> {
       String message = new String(delivery.getBody(), "UTF-8");
       String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
@@ -29,5 +24,21 @@ public class Consumer {
     };
     channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
     });
+  }
+
+  private static Channel connect() throws Exception {
+    while (true) {
+      try {
+        System.out.println("Connecting to RabbitMQ at host " + RABBITMQ_HOST);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(RABBITMQ_HOST);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        return channel;
+      } catch (Exception e) {
+        e.printStackTrace();
+        Thread.sleep(5000);
+      }
+    }
   }
 }
